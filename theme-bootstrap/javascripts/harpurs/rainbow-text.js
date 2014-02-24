15e529305fedc20d4ce1185638aad33f62b963f3
Andrew Wright <andrew.js.wright@gmail.com>
2014-02-24 20:35:53 +0000

    Added header logo back in. Updated the rainbow text script to match font colours to the brand more carefully

--- theme-bootstrap/javascripts/harpurs/rainbow-text.js
+++ theme-bootstrap/javascripts/harpurs/rainbow-text.js
@@ -3,12 +3,18 @@
   $.fn.rainbowize = function() {
     return this.each(function() {
       var rainbowtext = '';
+      var letter_count = 0;
       var index = 0;
  
       // get the current text inside element
       var text = $(this).text();
  
-      var colourArray = ["#31428E","#FC0D1B", "#94CE58", "#1EB1ED", "#FC0D1B"];
+      var colourArray = [
+        "#31428E", //navy
+        "#FC0D1B", //red
+        "#94CE58", //green
+        "#1EB1ED", //blue
+        "#FC0D1B"]; //red
 
       // hue is 360 degrees
       if (text.length > 0)
@@ -17,8 +23,13 @@
       // iterate the whole 360 degrees
       for (var i = 0; i < text.length; i++)
       {
-        index = i%colourArray.length;
+        index = letter_count%colourArray.length;
         rainbowtext = rainbowtext + '<span style="color:' + colourArray[index] + '">' + text.charAt(i) + '</span>';
+        if(text.charAt(i)==' ') {
+            letter_count = 0;
+        } else {
+          letter_count ++;
+        }
       }
  
       $(this).html(rainbowtext);
