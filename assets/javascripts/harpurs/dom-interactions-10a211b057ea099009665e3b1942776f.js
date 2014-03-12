27e8ef4e45f6458a59526c535d01812c937c61dd
Andrew Wright <andrew.js.wright@gmail.com>
2014-03-12 22:49:39 +0000

    master: 9c250da663c3f77fb33c55232ee86c2e620ae887 Updated made the nav bar skinny again and embedded the hill image in the footer

---
+++ assets/javascripts/harpurs/dom-interactions-10a211b057ea099009665e3b1942776f.js
@@ -0,0 +1,20 @@
+function getContentFromFirstContainer(matchClass) {
+    var elems = document.getElementsByTagName('*'), i;
+    debugger;
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+                > -1) {
+            return elems[i].innerHTML;
+        }
+    }
+}
+
+function replaceContentInContainer(matchClass,content) {
+    var elems = document.getElementsByTagName('*'), i;
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+                > -1) {
+            elems[i].innerHTML = content;
+        }
+    }
+}
