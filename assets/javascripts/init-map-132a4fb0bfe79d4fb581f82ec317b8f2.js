7fdf8bee4e266c1c85ddc84441fafd89cd1d625b
Andrew Wright <andrew.js.wright@gmail.com>
2014-04-13 18:30:21 +0100

    master: f0e7d7b521ff849afdebad261f9e36085bfeb039 Sorted out map and other locations sections of contact page

--- assets/javascripts/init-map-132a4fb0bfe79d4fb581f82ec317b8f2.js
+++
@@ -1,8 +0,0 @@
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(55.1357843, -6.6521336),
-    zoom: 8
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
